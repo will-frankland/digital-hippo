ced3b367476ff58a2023be107e2968596082f01e
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-22 13:30:32 -0400

    rename public, add click away and esc to navbar

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,15 +1,34 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { act, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const isAnyOpen = activeIndex !== null;
 
-  const isAnyOpen = activeIndex !== null
+  // Close Navbar when pressing Esc
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
+  // Close Navbar when clicking away
+  const navRef = useRef<HTMLDivElement | null>(null);
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, idx) => {
         const handleOpen = () => {
           if (activeIndex === idx) {
