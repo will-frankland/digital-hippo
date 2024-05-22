c0b63d0ba2afb737a299f747950e421ceb7c7585
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-22 12:15:06 -0400

    add logic to navbar styling with maps

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,14 +1,38 @@
-"use client"
+"use client";
 
-import { useState } from "react";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { act, useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
-  const [activeIndex, setActiveIndex] = useState<null | number>(null)
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  const isAnyOpen = activeIndex !== null
   return (
     <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, idx) => {
+        const handleOpen = () => {
+          if (activeIndex === idx) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(idx);
+          }
+        };
 
+        const isOpen = idx === activeIndex;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default NavItems;
