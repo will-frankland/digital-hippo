ff7a4b8c565ac854a18536f6715cf16b5093c925
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-23 03:23:25 -0400

    add logic for sign in screns on front and backend

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -12,12 +12,26 @@ import { trpc } from "@/trpc/client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useForm } from "react-hook-form";
-import { toast } from "sonner"
+import { toast } from "sonner";
 import { ZodError } from "zod";
 
 const Page = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const isSeller = searchParams.get("as") === "seller";
+  const origin = searchParams.get("origin");
+
+  const continueAsSeller = () => {
+    router.push("?as=seller");
+  };
+
+  const continueAsBuyer = () => {
+    router.replace("/sign-in", undefined);
+  };
+
   const {
     register,
     handleSubmit,
@@ -26,28 +40,33 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidator),
   });
 
-  const router = useRouter();
+  const { mutate: signIn, isLoading } = trpc.auth.signIn.useMutation({
+    onSuccess: () => {
+      toast.success("Signed in successfully");
 
-  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
-    onError: (err) => {
-      if(err.data?.code == "CONFLICT") {
-        toast.error("This email is alrady in use. Sign in instead?")
-        return
+      router.refresh();
+
+      // Will send user back to where they were after sign in (e.g. their cart)
+      if (origin) {
+        router.push(`/${origin}`);
+        return;
       }
-      if(err instanceof ZodError) {
-        toast.error(err.issues[0].message)
-        return
+      if (isSeller) {
+        router.push("/sell");
+        return;
       }
-      toast.error('Something went wrong. Please try again')
+
+      router.push("/");
     },
-    onSuccess: ({ sentToEmail }) => {
-      toast.success(`Verification email sent to ${sentToEmail}.`)
-      router.push('/verify-email?to=' + sentToEmail)
-    }
-  })
+    onError: (err) => {
+      if (err.data?.code === "UNAUTHORIZED") {
+        toast.error("Invalid email or password");
+      }
+    },
+  });
 
   const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
-    mutate({ email, password })
+    signIn({ email, password });
   };
 
   return (
@@ -56,15 +75,15 @@ const Page = () => {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center space-y-2 text-center">
             <Icons.logo className="h-20 w-20" />
-            <h1 className="text-2xl font-bold">Create an account</h1>
+            <h1 className="text-2xl font-bold">Sign in to your {isSeller ? "seller" : ''} account</h1>
             <Link
-              href="/sign-in"
+              href="/sign-up"
               className={buttonVariants({
                 variant: "link",
                 className: "gap-1.5",
               })}
             >
-              Already have an account?
+              Don&apos;t have an account?
               <ArrowRightIcon className="h-4 w-4" />
             </Link>
           </div>
@@ -81,7 +100,9 @@ const Page = () => {
                     placeholder="you@example.ca"
                   />
                   {errors?.email && (
-                    <p className="text-sm text-red-500">{errors.email.message}</p>
+                    <p className="text-sm text-red-500">
+                      {errors.email.message}
+                    </p>
                   )}
                 </div>
               </div>
@@ -97,12 +118,44 @@ const Page = () => {
                     placeholder="Password"
                   />
                   {errors?.password && (
-                    <p className="text-sm text-red-500">{errors.password.message}</p>
+                    <p className="text-sm text-red-500">
+                      {errors.password.message}
+                    </p>
                   )}
                 </div>
-                <Button>Sign up</Button>
+                <Button>Sign in</Button>
               </div>
             </form>
+            <div className="relative">
+              <div
+                aria-hidden="true"
+                className="absolute inset-0 flex items-center"
+              >
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  or
+                </span>
+              </div>
+            </div>
+            {isSeller ? (
+              <Button
+                onClick={continueAsBuyer}
+                variant="secondary"
+                disabled={isLoading}
+              >
+                Continue as customer
+              </Button>
+            ) : (
+              <Button
+                onClick={continueAsSeller}
+                variant="secondary"
+                disabled={isLoading}
+              >
+                Continue as seller
+              </Button>
+            )}
           </div>
         </div>
       </div>
--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -6,7 +6,8 @@ const handler = (req: Request) => {
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: () => ({})
+    // @ts-expect-error context alrady passed from express middleware
+    createContext: () => ({}),
   })
 }
 
