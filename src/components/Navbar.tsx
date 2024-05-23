e33768717a93617427f1fb461cb0c6e6e27e9359
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-23 03:53:23 -0400

    add logout functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { buttonVariants } from "./ui/button";
 import Cart from "./Cart";
 import { getServerSideUser } from "@/lib/payload-utils";
 import { cookies } from "next/headers";
+import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async () => {
   const nextCookies = cookies()
@@ -41,7 +42,7 @@ const Navbar = async () => {
                     <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
                   )}
                   {user ? (
-                    <p></p>
+                    <UserAccountNav user={user} />
                   ) : (
                     <Link
                       href="/sign-up"
--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,11 +1,45 @@
-"use client"
+"use client";
 
-const UserAccountNav = () => {
+import {
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { User } from "@/payload-types";
+import Link from "next/link";
+import { useAuth } from "@/hooks/use-auth";
+
+const UserAccountNav = ({ user }: { user: User }) => {
+  const { signOut } = useAuth();
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="overflow-visible">
+        <Button variant="ghost" size="sm" className="relative">
+          My account
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white w-60" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-0.5 leading-none">
+            <p className="font-medium text-sm text-black">{user.email}</p>
+          </div>
+        </div>
+        <DropdownMenuSeparator />
 
-export default UserAccountNav
+        <DropdownMenuItem asChild>
+          <Link href="/sell">Seller Dashboard</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer" onClick={signOut}>
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserAccountNav;
