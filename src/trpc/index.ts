aa89d0d06738e6e81f6a642cad1b6d1165ddd661
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-22 21:58:15 -0400

    setup trpc

---
+++ src/trpc/index.ts
@@ -0,0 +1,9 @@
+import { router, publicProcedure } from "./trpc"
+
+export const appRouter = router({
+  anyApiRoute: publicProcedure.query(() => {
+    return "Hello"
+  })
+})
+
+export type AppRouter = typeof appRouter
