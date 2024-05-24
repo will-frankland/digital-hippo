14b66910e733ed02a175134759e84979de93bbe6
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-24 14:10:17 -0400

    add code to create new product for stripe

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -2,27 +2,72 @@ import { z } from "zod";
 import { privateProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { getPayloadClient } from "../get-payload";
+import { stripe } from "../lib/stripe";
+import type Stripe from "stripe";
 
 export const PaymentRouter = router({
-  createSession: privateProcedure.input(z.object({ productIds: z.array(z.string()) }))
-  .mutation(async ({ ctx, input }) => {
-    const { user } = ctx
-    let { productIds } = input
+  createSession: privateProcedure
+    .input(z.object({ productIds: z.array(z.string()) }))
+    .mutation(async ({ ctx, input }) => {
+      const { user } = ctx;
+      let { productIds } = input;
 
-    if(productIds.length === 0) {
-      throw new TRPCError({ code: "BAD_REQUEST" })
-    }
-
-    const payload = await getPayloadClient()
-
-    const { docs: products } = await payload.find({
-      collection: "products",
-      where: {
-        id: {
-          in: productIds
-        }
+      if (productIds.length === 0) {
+        throw new TRPCError({ code: "BAD_REQUEST" });
       }
-      
-    })
-  }),
-})
+
+      const payload = await getPayloadClient();
+
+      const { docs: products } = await payload.find({
+        collection: "products",
+        where: {
+          id: {
+            in: productIds,
+          },
+        },
+      });
+
+      const filteredProducts = products.filter((prod) => Boolean(prod.priceId));
+
+      const order = await payload.create({
+        collection: "orders",
+        data: {
+          _isPaid: false,
+          products: filteredProducts,
+          user: user.id,
+        },
+      });
+
+      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = []
+
+      filteredProducts.forEach((product) => {
+        line_items.push({
+          price: product.priceId!,
+          quantity: 1
+        })
+      })
+
+      line_items.push({
+        // Transaction Fee setup on stripe @ $1
+        price: "price_1PK2F5LjxulCfhP502T1sldD",
+        quantity: 1,
+        adjustable_quantity: {
+          enabled: false
+        }
+      })
+
+      try {
+        const stripeSession = await stripe.checkout.sessions.create({
+          success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+          cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
+          payment_method_types: ["card", "paypal"],
+          mode: "payment",
+          metadata: {
+            userId: user.id,
+            orderId: order.id,
+          },
+          line_items,
+        });
+      } catch (error) {}
+    }),
+});
