d09b0165a553eb0947ead10f79401321532d1ae9
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-23 23:12:20 -0400

    add swiper functioanlity for multiple images and bullet pagination for image cards

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -6,7 +6,7 @@ import "swiper/css/pagination";
 import type SwiperType from "swiper";
 import { Pagination } from "swiper/modules";
 import { cn } from "@/lib/utils";
-import { ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 interface ImageSliderProps {
   urls: string[];
@@ -30,8 +30,6 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
     });
   }, [swiper, urls]);
 
-  console.log("urls", urls);
-
   const activeStyles =
     "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
 
@@ -41,16 +39,40 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
     <div className="group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
       <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
         <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slideNext();
+          }}
           className={cn(activeStyles, "right-3 transition", {
             [inactiveStyles]: slideConfig.isEnd,
             "hover:bg-primary-300 text-primary-800 opacity-100":
               !slideConfig.isEnd,
           })}
           aria-label="next image"
-        ><ChevronRight className="h-4 w-4 text-zinc-700" /></button>
-        <button></button>
+        >
+          <ChevronRight className="h-4 w-4 text-zinc-700" />{" "}
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slidePrev();
+          }}
+          className={cn(activeStyles, "left-3 transition", {
+            [inactiveStyles]: slideConfig.isBeginning,
+            "hover:bg-primary-300 text-primary-800 opacity-100":
+              !slideConfig.isBeginning,
+          })}
+          aria-label="previous image"
+        >
+          <ChevronLeft className="h-4 w-4 text-zinc-700" />{" "}
+        </button>
       </div>
       <Swiper
+        pagination={{
+          renderBullet: (_, className) => {
+            return `<span class="rounded-full transition ${className}"></span>`
+          }
+        }}
         onSwiper={(swiper) => setSwiper(swiper)}
         spaceBetween={50}
         modules={[Pagination]}
