a10b3e15eb8d21978de5070f86b63a4581713754
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-24 14:12:22 -0400

    add code to update price on stripe products

--- src/collections/products/Products.ts
+++ src/collections/products/Products.ts
@@ -2,13 +2,13 @@ import { BeforeChangeHook } from "payload/dist/collections/config/types";
 import { PRODUCT_CATEGORIES } from "../../config";
 import { CollectionConfig } from "payload/types";
 import { Product } from "../../payload-types";
-import { stripe } from '../../lib/stripe'
+import { stripe } from "../../lib/stripe";
 
 const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
-  const user = req.user
+  const user = req.user;
 
-  return {...data, user: user.id}
-}
+  return { ...data, user: user.id };
+};
 
 export const Products: CollectionConfig = {
   slug: "products",
@@ -18,9 +18,10 @@ export const Products: CollectionConfig = {
   access: {},
   hooks: {
     beforeChange: [
-      addUser, async (args) => {
-        if(args.operation === 'create') {
-          const data = args.data as Product
+      addUser,
+      async (args) => {
+        if (args.operation === "create") {
+          const data = args.data as Product;
 
           const createdProduct = await stripe.products.create({
             name: data.name,
@@ -28,19 +29,30 @@ export const Products: CollectionConfig = {
               currency: "CAD",
               unit_amount: Math.round(data.price * 100),
             },
-          })
+          });
           const updated: Product = {
             ...data,
             stripeId: createdProduct.id,
-            priceId: createdProduct.default_price as string
-          }
-          return updated
-          
-        } else if(args.operation === "update") {
+            priceId: createdProduct.default_price as string,
+          };
+          return updated;
+        } else if (args.operation === "update") {
+          const data = args.data as Product;
 
+          const updatedProduct = await stripe.products.update(data.stripeId!, {
+            name: data.name,
+            default_price: data.priceId!
+          })
+
+          const updated: Product = {
+            ...data,
+            stripeId: updatedProduct.id,
+            priceId: updatedProduct.default_price as string,
+          };
+          return updated;
         }
-      }
-    ]
+      },
+    ],
   },
   fields: [
     {
