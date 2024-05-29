8d7faebe2c8b50a276db8e4fb110c13475f9c605
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-29 13:31:27 -0400

    render order details and add fn to calculate totals

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import { cookies } from "next/headers";
 import { getPayloadClient } from "@/get-payload";
 import { notFound, redirect } from "next/navigation";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { Product, ProductFile } from "@/payload-types";
+import { formatPrice } from "@/lib/utils";
 
 interface PageProps {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -32,9 +35,15 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
   const orderUserId =
     typeof order.user === "string" ? order.user : order.user.id;
 
-    if(orderUserId !== user?.id) {
-      return redirect(`/sign-in?origin=thank-you?orderId=${order.id}`)
-    }
+  if (orderUserId !== user?.id) {
+    return redirect(`/sign-in?origin=thank-you?orderId=${order.id}`);
+  }
+
+  const products = order.products as Product[];
+
+  const orderTotal = products.reduce((total, product) => {
+    return total + product.price;
+  }, 0);
 
   return (
     <main className="relative lg:mix-h-full">
@@ -54,6 +63,78 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
           <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
             Thanks for ordering
           </h1>
+          {order._isPaid ? (
+            <p className="mt-2 text-base text-muted-foreground">
+              Your order was processed and your assets are available to download
+              below. We&apos;ve sent your receipt and order details to{" "}
+              {typeof order.user !== "string" ? (
+                <span className="font-medium text-gray-900">
+                  {order.user.email}
+                </span>
+              ) : null}
+              .
+            </p>
+          ) : (
+            <p className="mt-2 text-base text-muted-foreground">
+              We&apos;re currently processing it. Hang tight, this won&apos;
+              take long. We&apos;ll send confirmation very soon!
+            </p>
+          )}
+          <div className="mt-16 text-sm font-medium">
+            <div className="text-muted-foreground">Order Number:</div>
+            <div className="mt-2 text-gray-900">{order.id}</div>
+            <ul className="mt-6 divide-y divide-gray-200 border-top border-gray-200 text-sm font-medium text-muted-foreground">
+              {(order.products as Product[]).map((product) => {
+                const label = PRODUCT_CATEGORIES.find(
+                  ({ value }) => value === product.category
+                )?.label;
+
+                const downloadUrl = (product.product_files as ProductFile)
+                  .url as string;
+
+                const { image } = product.images[0];
+
+                return (
+                  <li key={product.id} className="flex space-x-6 py-6">
+                    <div className="relative h-24 w-24">
+                      {typeof image !== "string" && image.url ? (
+                        <Image
+                          fill
+                          src={image.url}
+                          alt={`${product.name} image`}
+                          className="flex-none rounded-medium bg-gray-100 object-cover object-center"
+                        />
+                      ) : null}
+                    </div>
+                    <div className="flex-auto flex flex-col justify-between">
+                      <div className="space-y-1">
+                        <h3 className="text-gray-900">{product.name}</h3>
+                        <p className="my-1">Category: {label}</p>
+                      </div>
+                      {order._isPaid ? (
+                        <a
+                          href={downloadUrl}
+                          download={product.name}
+                          className="text-purple-600 hover:underline underline-offset-2"
+                        >
+                          Download Asset
+                        </a>
+                      ) : null}
+                    </div>
+                    <p className="flex-none font-medium text-gray-900">
+                      {formatPrice(product.price)}
+                    </p>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-muted-foreground">
+              <div className="flex justify-between">
+                <p>Subtotal</p>
+                <p>{formatPrice(orderTotal)}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
