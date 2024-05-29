57a74421cc0a17286ae5aa80508b652481440927
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-29 13:09:13 -0400

    setup thank you page and query database which searchParams to grab orderId for DB

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,57 @@
+import { getServerSideUser } from "@/lib/payload-utils";
+import Image from "next/image";
+import { cookies } from "next/headers";
+import { getPayloadClient } from "@/get-payload";
+import { notFound } from "next/navigation";
+
+interface PageProps {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+const ThankYouPage = async ({ searchParams }: PageProps) => {
+  
+  const orderId = searchParams.orderId
+  const nextCookies = cookies()
+
+  const { user } = await getServerSideUser(nextCookies)
+  const payload = await getPayloadClient()
+
+  const { docs: orders } = await payload.find({
+    collection: "orders",
+    depth: 2,
+    where: {
+      id: {
+        equals: orderId
+      }
+    }
+  })
+
+  const [order] = orders
+
+  if(!order) return notFound()
+
+  return (
+    <main className="relative lg:mix-h-full">
+      <div className="hidden lg:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
+        <Image
+          fill
+          src="/checkout-thank-you.jpg"
+          className="h-full w-full object-cover object-center"
+          alt="Thank you for your order"
+        />
+      </div>
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24">
+        <div className="lg:col-start-2">
+          <p className="text-sm font-medium text-purple-600">
+            Order successful
+          </p>
+          <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Thanks for ordering
+          </h1>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default ThankYouPage;
