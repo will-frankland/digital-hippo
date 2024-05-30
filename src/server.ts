f9333af9b30308f08c2a09908a4bbb5dd550175a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-30 15:06:23 -0400

    solve build errors for prod

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import { inferAsyncReturnType } from "@trpc/server";
 import bodyParser from "body-parser";
 import { IncomingMessage } from "http";
 import { stripeWebhookHandler } from "./webhooks";
+import nextBuild from "next/dist/build";
+import path from "path";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -19,20 +21,19 @@ const createContext = ({
   res,
 });
 
-export type ExpressContext = inferAsyncReturnType<typeof createContext>
+export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 
-export type WebhookRequest = IncomingMessage & { rawBody: Buffer }
+export type WebhookRequest = IncomingMessage & { rawBody: Buffer };
 
 const start = async () => {
-  
-  // Used to verify payment from Stripe 
+  // Used to verify payment from Stripe
   const webhookMiddleware = bodyParser.json({
-    verify: (req: WebhookRequest,  _, buffer) => {
-      req.rawBody = buffer
-    }
-  })
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer;
+    },
+  });
 
-  app.post('/api/webhooks/stripe', webhookMiddleware, stripeWebhookHandler)
+  app.post("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler);
 
   const payload = await getPayloadClient({
     initOptions: {
@@ -42,6 +43,19 @@ const start = async () => {
       },
     },
   });
+
+  if(process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info("NextJs is building for production")
+
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, '../'))
+
+      process.exit();
+    })
+
+    return
+  }
 
   app.use(
     "/api/trpc",
