0cae434f77d33dfb32e024fc134445a263fdca6e
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-30 16:08:50 -0400

    add code to sync user to product when product is created

--- src/collections/products/Products.ts
+++ src/collections/products/Products.ts
@@ -1,4 +1,7 @@
-import { BeforeChangeHook } from "payload/dist/collections/config/types";
+import {
+  AfterChangeHook,
+  BeforeChangeHook,
+} from "payload/dist/collections/config/types";
 import { PRODUCT_CATEGORIES } from "../../config";
 import { CollectionConfig } from "payload/types";
 import { Product } from "../../payload-types";
@@ -8,6 +11,35 @@ const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
   const user = req.user;
 
   return { ...data, user: user.id };
+};
+
+const syncUser: AfterChangeHook<Product> = async ({ req, doc }) => {
+  const fullUser = await req.payload.findByID({
+    collection: "users",
+    id: req.user.id,
+  });
+
+  if (fullUser && typeof fullUser === "object") {
+    const { products } = fullUser;
+    const allIDs = [
+      ...(products?.map((product) =>
+        typeof product === "object" ? product.id : product
+      ) || []),
+    ];
+    const createdProductIDs = allIDs.filter(
+      (id, idx) => allIDs.indexOf(id) === idx
+    );
+
+    const dataToUpdate = [...createdProductIDs, doc.id];
+
+    await req.payload.update({
+      collection: "users",
+      id: fullUser.id,
+      data: {
+        products: dataToUpdate,
+      },
+    });
+  }
 };
 
 export const Products: CollectionConfig = {
@@ -41,8 +73,8 @@ export const Products: CollectionConfig = {
 
           const updatedProduct = await stripe.products.update(data.stripeId!, {
             name: data.name,
-            default_price: data.priceId!
-          })
+            default_price: data.priceId!,
+          });
 
           const updated: Product = {
             ...data,
