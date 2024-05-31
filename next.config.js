608963261cb6200e7dba2d6fe16ffb222ffffcc9
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-31 11:14:53 -0400

    change next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/media/**",
-      },
+    domains: [
+      "localhost",
+      "https://digital-hippo-production-037e.up.railway.app",
     ],
   },
 };
