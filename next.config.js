3164dc27730eabc326b9d5b082c73d411ea503d1
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-06-03 18:44:35 -0400

    add deployment url to next config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/media/**",
-      },
-      {
-        hostname: 'https://digital-hippo-production-037e.up.railway.app'
-      }
-    ],
-  },
+    domains: [
+      'localhost',
+      'https://digital-hippo-production-037e.up.railway.app'
+    ]
+  }
 };
 
 module.exports = nextConfig;
